6afb662db36ff81e20aa806d55dfcec3d2f071bf
Piotrek <piotrekste98@gmail.com>
2020-11-05 11:08:40 +0100

    changes in timetable component

--- frontend/src/components/Timetable/TimetableContent.jsx
+++ frontend/src/components/Timetable/TimetableContent.jsx
@@ -1,8 +1,35 @@
 import React, { Component } from "react";
 
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import TodaysTasks from "./TodaysTasks";
+const localizer = momentLocalizer(moment);
+
 class TimetableContent extends Component {
+  state = {
+    events: [
+      {
+        start: moment().toDate(),
+        end: moment().add(1, "days").toDate(),
+        title: "Some title",
+      },
+    ],
+  };
   render() {
-    return <div className="container"> timetable</div>;
+    return (
+      <div className="container">
+        <Calendar
+          className="calendar-container"
+          localizer={localizer}
+          defaultDate={new Date()}
+          defaultView="month"
+          events={this.state.events}
+          style={{ height: "85vh" }}
+        />
+        <TodaysTasks />
+      </div>
+    );
   }
 }
 
---
+++ frontend/src/components/Timetable/TodaysTasks.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+
+class TodaysTasks extends Component {
+  render() {
+    return (
+      <div className="extras-container">
+        <br /> tutaj np dzisiejsze zadania
+        <br /> <br /> <br /> <br /> <br /> <br />
+        dzisiejsze spotkania
+        <br /> <br /> <br /> <br /> <br />
+      </div>
+    );
+  }
+}
+
+export default TodaysTasks;
+
+/**
+ *    <Segment>
+            <Divider horizontal></Divider>
+
+            <Button
+              color="teal"
+              content="EDYTUJ"
+              icon="add"
+              labelPosition="left"
+            />
+            <Divider horizontal></Divider>
+            <Button
+              color="teal"
+              content="ZAPLANUJ"
+              icon="add"
+              labelPosition="left"
+            />
+          </Segment>
+ */
