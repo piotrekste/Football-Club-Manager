57b8f4b84708b3d556ee222a79b8d75a0ef03a28
Piotrek <piotrekste98@gmail.com>
2020-11-05 11:08:22 +0100

    changes in staff component

--- frontend/src/components/Staffs/StaffsContent.jsx
+++ frontend/src/components/Staffs/StaffsContent.jsx
@@ -1,8 +1,40 @@
 import React, { Component } from "react";
-
+import setHeaders from "../../utils/setHeaders";
+//import PlayersModal from "./PlayersModal";
+import { Card } from "semantic-ui-react";
+import foto from "../../img/patrick.png";
 class StaffsContent extends Component {
+  state = {
+    staffs: [],
+  };
+  getAllStaffs = async () => {
+    const response = await fetch("http://localhost:5000/staffs/", setHeaders());
+    const body = await response.json();
+    this.setState({ staffs: body });
+    console.log("data", this.state.staffs);
+  };
+  componentDidMount = async () => {
+    await this.getAllStaffs();
+  };
   render() {
-    return <div className="container"> staff</div>;
+    return (
+      <div className="container">
+        {" "}
+        <Card.Group itemsPerRow={4}>
+          {this.state.staffs.map((value, key) => (
+            <Card
+              key={key}
+              className="staffs-card"
+              image={foto}
+              header={value.first_name + " " + value.last_name}
+              meta={value.role.toUpperCase()}
+              //extra={<PlayersModal players={this.state.players} />}
+              //tutaj zaplanuj
+            />
+          ))}
+        </Card.Group>
+      </div>
+    );
   }
 }
 
